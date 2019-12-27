08f2ab2875e54449f95c71e724db1564728df66b
eljefeo <jeffnavabiz@yahoo.com>
2019-12-27 00:05:21 -0800

    update

--- src/Problem15.java
+++ src/Problem15.java
@@ -4,16 +4,16 @@ public class Problem15 {
 		problem();
 
 	}
+	
+	/*Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+	How many such routes are there through a 20×20 grid?*/
 
 	private static void problem() {
-		int height = 21;//, length = 10;
+		int height = 21;
 		long[][] arr = new long[height][height];
-		long[][] vals = new long[height][height];
 		
 		for(int i = 0; i < height; i++){
 			arr[i][0] = 1;
-		}
-		for(int i = 0; i < height; i++){
 			arr[0][i] = 1;
 		}
 		
