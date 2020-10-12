57795476d276454024a69b5b481eeb19dce4edef
eljefeo <jeffnavabiz@yahoo.com>
2020-10-12 00:38:52 -0700

    Prob 48 done - using bigint lol

---
+++ src/Problem48.java
@@ -0,0 +1,30 @@
+import java.math.BigInteger;
+
+public class Problem48 {
+
+	/*
+	 The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+
+		Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+		
+	 */
+	
+	
+	public static void main(String[] args) {
+		problem();
+	}
+	
+	private static void problem(){
+		int max = 1001;
+		long digits = 10000000000L;
+		BigInteger b = new BigInteger("0");
+		for(int i = 1; i < max; i++){
+			BigInteger c = new BigInteger(""+i).pow(i);
+			b = b.add(c);
+		}
+		//.....789498383642350667978127819110846700
+		System.out.println("Last ten digits: " + b.mod(new BigInteger(""+digits)));
+		
+	}
+
+}
