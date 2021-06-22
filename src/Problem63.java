d31f4f7e7b46f2188a681ea6cc9f78c5205ad373
eljefeo <jeffnavabiz@yahoo.com>
2021-06-22 01:04:20 -0700

    62 solved by doing a lot - dont know when to stop though

--- src/Problem63.java
+++ src/Problem63.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 
 public class Problem63 {
 	
@@ -8,7 +9,42 @@ public class Problem63 {
 	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//solved by just doing a whole bunch and seeing what the number is at the end
+		// dont know when to stop though....
+		int min = 1;
+		int max = 100000;
+		int count = 0;
+		int maxExponent = 50;
+		//int targetNumberOfCubes = 5;
+		
+		//Map<Long, Integer> alln = new HashMap<Long, Integer>();
+		
+		for(int i = min; i < max; i++) {
+			
+			
+			if(i%1000 == 0) { // just for logging
+				System.out.println("Doing " + i + "....");
+			}
+			
+			BigInteger b1 = new BigInteger("" + i);
+			
+			for(int j=1; j<=maxExponent; j++) {
+				BigInteger b2 = b1.pow(j);
+				
+				if(b2.toString().length() == j) {
+					count++;
+					
+					//if(count % 10 == 0) {
+						System.out.println("Count at " + count + " for i=" + i + " and exponent=" + j + " for " + b2);
+						
+					//}
+					
+				}
+			}
+			
+		}
+		
+		System.out.println("FINAL Count at " + count); 
 
 	}
 
