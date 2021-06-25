76aa1324622bef5e582f5d5e62f6448a5dd594d8
eljefeo <jeffnavabiz@yahoo.com>
2021-06-24 19:16:30 -0700

    start 65

---
+++ src/Problem65.java
@@ -0,0 +1,19 @@
+
+public class Problem65 {
+
+	
+	/*
+	 Convergents of e:
+	 
+	 The square root of 2 can be written as an infinite continued fraction.
+	 
+	 
+	 
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
