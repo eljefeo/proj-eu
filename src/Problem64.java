e1f6d0a9869a44f50bdd4631504a350988292111
eljefeo <jeffnavabiz@yahoo.com>
2021-06-24 19:14:42 -0700

    64 done

--- src/Problem64.java
+++ src/Problem64.java
@@ -7,103 +7,71 @@ public class Problem64 {
 	//Odd period square roots
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		problem	();
+		problem();
 	}
 	
 	private static void problem() {
-		int nnn = 23;
-		double sn = Math.sqrt(nnn);
-		int firstWholeNum = (int) Math.sqrt(nnn);
 		
-		
-		int finalTopWholeNum = firstWholeNum * -1;
-		int firstBottom = 1;
-		//int firstNum = -4;
-		//int firstNumer = 1;
-		
-		//int firstTopNum = firstNum * -1 ; // + 3
-		/// started with 1 / sq23-4
-		//int firstBottom = nnn - (firstTopNum * firstTopNum); // 23 - 9 = 14
-		/////SHOULD CHECK HERE IF IT SIMPLIFIES TO 1 BECAUSE WE NEED TO DO firstNumer / firstBottom
-		//double firstBottomD = firstBottom / firstNumer;
-		//if(firstBottomD - (int)firstBottomD != 0) {
-		//	System.out.println("ERROR .... with " + firstBottomD + "  for first " +  firstNum + " first numer " + firstNumer);
-		//}
-		
-		//firstBottom = (int) firstBottomD;
-		//int extractNextWholeNum = (int) (sn + firstTopNum) / firstBottom;
-		//int finalTopWholeNum = firstTopNum - (extractNextWholeNum * firstBottom); // should be 4 - (1*7) = -3
-		//System.out.println("111 next line = sq23  + "  + finalTopWholeNum + " / " + firstBottom + " extractedwholenum: " + extractNextWholeNum);
-		
-		for(int i = 0; i < 6; i++) {
-			int firstNum = finalTopWholeNum;
-			int firstNumer = firstBottom;
-			int firstTopNum = firstNum * -1 ; // + 3
-			firstBottom = nnn - (firstTopNum * firstTopNum); // 23 - 9 = 14
-			double firstBottomD = firstBottom / firstNumer;
-			if(firstBottomD - (int)firstBottomD != 0) {
-				System.out.println("ERROR .... with " + firstBottomD + "  for first " +  firstNum + " first numer " + firstNumer);
+		int max = 10000;
+		int oddPerCounter = 0;
+
+		for(int i = 2; i < max; i++) {
+			
+			double sn = Math.sqrt(i);
+			int firstWholeNum = (int) sn;
+			
+			if(sn - firstWholeNum == 0) {
+				continue; // dont do normal squares...
 			}
-			firstBottom = (int) firstBottomD;
-			int extractNextWholeNum = (int) (sn + firstTopNum) / firstBottom;
-			finalTopWholeNum = firstTopNum - (extractNextWholeNum * firstBottom); // should be 4 - (1*7) = -3
-			System.out.println("next line = sq23  + "  + finalTopWholeNum + " / " + firstBottom + " extractedwholenum: " + extractNextWholeNum);
+			
+			firstWholeNum = firstWholeNum * -1;
+			int firstBottom = 1;
+			
+			List<Integer[]> track = new ArrayList<Integer[]>();
+			
+			track.add(new Integer[] {firstWholeNum, firstBottom} );
+			
+			boolean foundPeriod = false;
+			while(!foundPeriod) {
+				int firstNumer = firstBottom; // do reciprocal first, numer becomes denom for next iteration
+				firstWholeNum = firstWholeNum * -1 ; // + 3
+				firstBottom = i - (firstWholeNum * firstWholeNum); // 23 - 9 = 14
+				double firstBottomD = firstBottom / firstNumer;
+				
+				// check if numer and denom can be simplified down to 1 on the numerator (like 7/14 or 5/20 ... error if something like 9/12)
+				if(firstBottomD - (int)firstBottomD != 0) {
+					System.out.println("ERROR .... with " + firstBottomD + "  for first " +  firstWholeNum + " first numer " + firstNumer);
+					return;
+				}
+				
+				firstBottom = (int) firstBottomD;
+				int extractNextWholeNum = (int) (sn + firstWholeNum) / firstBottom;
+				firstWholeNum = firstWholeNum - (extractNextWholeNum * firstBottom); // should be 4 - (1*7) = -3
+				
+				// Check for period:
+				Integer[] current = new Integer[] {firstWholeNum, firstBottom};
+				for(int j=0; j< track.size();j++) {
+					Integer[] ia = track.get(j);
+					if(ia.length == 2 && ia[0] == current[0] && ia[1] == current[1]) {
+						int periodSize = track.size() - j;
+
+						if(periodSize > 0 && periodSize % 2 != 0) {
+							oddPerCounter++;
+						}
+						foundPeriod = true;
+						break;
+					}
+				}
+				
+				track.add(current);
+				
+			}
 		}
 		
-		
-		
-	}
-	
-	private static void problem2() {
-		int nnn = 23;
-		double sn = Math.sqrt(nnn);
-		int firstWholeNum = (int) Math.sqrt(nnn);
-		
-		int firstNumer = 1;
-		
-		int firstNum = firstWholeNum * -1; // - 4
-		
-	
-		
-		
-		int firstTopNum = firstNum * -1 ; // +4
-		/// started with 1 / sq23-4
-		int firstBottom = nnn - (firstTopNum * firstTopNum); // 7
-		
-		// now we have sq23 + 4 on top, and 7 on bottom
-		
-		
-		int extractNextWholeNum = (int) (sn + firstTopNum) / firstBottom;
-		int finalTopWholeNum = firstTopNum - (extractNextWholeNum * firstBottom); // should be 4 - (1*7) = -3
-		//System.out.println("nnn "  + nnn);
-		//System.out.println("firstWholeNum "  + firstWholeNum);
-		//System.out.println("firstNum "  + firstNum);
-		//System.out.println("firstTopNum "  + firstTopNum);
-		//System.out.println("firstBottom "  + firstBottom);
-		//System.out.println("extractNextWholeNum "  + extractNextWholeNum);
-		//System.out.println("finalTopWholeNum "  + finalTopWholeNum);
-		System.out.println("next line = sq23  + "  + finalTopWholeNum + " / " + firstBottom + " extractedwholenum: " + extractNextWholeNum);
-		
-		
-		
-		
-		
-		
-		
-		// now we ahve extractNextWholeNum = 1
-		// and sq23 + finalTopWholeNum (which is -3) / firstBottom (which is 7)
-		
-		//recip and repeat?
-		// so 7 / sq23 - 3
-		
-		// next we do 
-		
-		int nexttop = finalTopWholeNum; // -3
-		
+		System.out.println("Odd period count under " + max + " :: " + oddPerCounter);
 		
 	}
+
 	/*	
 		//sq23 = 4.795831523312719
 		 * int wholenum = (int) sq23;
@@ -162,35 +130,7 @@ public class Problem64 {
 		
 		*/
 		
-		/*
-		double s = Math.sqrt(23);
-		int si = (int) s;
-		System.out.println("s23 : " + s + " , and int: " + si);
-		
-		
-		List<Double> parts = new ArrayList<Double>();
-		
-		for(int i=0; i<25; i++) {
-			//double ss =  1 / (s - si);
-			s =  1 / (s - si);
-			if(parts.contains(s)) {
-				//System.out.println("Has it already : " + s);
-			} else {
-				//System.out.println("Does not have it yet : " + s);
-				parts.add(s);
-			}
-			//System.out.println("s : " + s );
-			
-			if( s < 1.26 && s > 1.12) {
-				System.out.println("Hasrty : " + s);
-			}
-			
-			si = (int) s;
-			System.out.println("si : " + si );
-		}
-		
-		*/
-		
+	
 	//}
 
 }
