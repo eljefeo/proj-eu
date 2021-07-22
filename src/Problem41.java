9bbbca109b7f3b10b9f35dcb034b1560237a4839
eljefeo <jeffnavabiz@yahoo.com>
2021-07-22 01:35:03 -0700

    make 41 faster

--- src/Problem41.java
+++ src/Problem41.java
@@ -1,5 +1,5 @@
 
-public class Problem41 {
+public class Problem41 extends ProblemImpl{
 
 	/*
 	 We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once. For example, 2143 is a 4-digit pandigital and is also prime.
@@ -7,11 +7,12 @@ public class Problem41 {
 What is the largest n-digit pandigital prime that exists?
 	 */
 	
-	public static void main(String[] args) {
-		problem();
+	public static void main(String[] args) { // TAKES 12 sec
+		Problem p = new Problem41();
+		p.runProblem();
 	}
 	
-	private static void problem(){
+	public void problem(){
 		int test  = 6582143;
 		/*boolean isNthDigitPandigital = isNthDigitPandigitalNumber(test);
 		boolean isPrime = Util.isPrime(test);
@@ -19,21 +20,19 @@ What is the largest n-digit pandigital prime that exists?
 		System.out.println(test + " is prime? " + isPrime);*/
 		int max = 99999999;
 		int solution = 0;
-		int step = 1000000;
+		int step = 10000000;
 		for(int i=0; i<max; i++){
+			
 			if(i > step && i%step==0) System.out.println("At step " + i);
-			boolean isNthDigitPandigital = Util.isNthDigitPandigitalNumber(i);
-			boolean isPrime = Util.isPrime(i);
-			if(isNthDigitPandigital && isPrime){
-				//System.out.println(i + " is nth digit pandigital and prime? " + (isNthDigitPandigital && isPrime));
-				if(i > solution)
-					solution = i;
+			
+			if(!Util.isNthDigitPandigitalNumber(i) || !Util.isPrime(i)) {
+				continue;
 			}
+			
+			if(i > solution)
+					solution = i;
 		}
 		System.out.println("Max? " + solution);
-		//854213 for 999999
-		//7652413 for 9999999;
-		
 	}
 	
 
