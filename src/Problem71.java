74fe8a227cb3d983c6b9d4888ca52baf33d83b53
eljefeo <jeffnavabiz@yahoo.com>
2021-07-28 01:03:21 -0700

    71 done

--- src/Problem71.java
+++ src/Problem71.java
@@ -17,61 +17,116 @@ By listing the set of reduced proper fractions for d <= 1,000,000 in ascending o
 	 
 	 */
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) { //Took 0.029194 seconds
 		new Problem71().runProblem();
 	}
-
 	/*
-	1/8, 1/7, 1/6, 1/5, 
+	 
+	 so after looking at these fractions for a while here's what Im thinking
+	 3/7 is next to 2/5 because:
+	 we work our way down the denoms from the highest (8,7,6,5...)
+	 until we find a fraction where the reduced fraction has  a denom less than our max (8)
+	 so:
+	 3/7 -> 8
+	 lcm of 7 and 8 = 56
+	 so 3/7 = 8/56
+	 this means the fraction directly to the left would be 7/56
+	 can we reduce 7/56 to the point where the denom is <8?
+	 no...
+	 
+	 so go to the next one (skip 7 since we are doing 7)
+	 7 and 6 = 42
+	 so 3/7 = 18/42
+	 so we need 17/42
+	 can we reduce 42 below 8? No...
+	 on to 5...
+	 7 and 5 = 35
+	 so 15/35 means we need 14/35
+	 hmm both are divisible by 7
+	 14/35 = 2/5
+	 5 is below 8, there we go...
+	 answer is 2/5
+	 lets try it
+	 
+	 
+	 
+	 */
 	
-	*1/4 or 2/8, 
-	
-	2/7, 
-	
-	*1/3 or 2/6, 
-	
-	3/8, 2/5, 3/7, 
-	
-	*1/2 or 3/6 or 4/8, 
-	
-	4/7, 3/5, 5/8, 
-	
-	*2/3 or 4/6, 
-	
-	5/7, 
-	
-	*3/4 or 6/8, 
-	
-	4/5, 5/6, 6/7, 7/8
 
-	*/
 	
+	
+	public long[] lcmWithMult(long a, long b) { //assuming both nums are positive
+		
+		long h, l;
+		if(a == b) 
+			return new long[] { a, 1 };
+		
+		if(a < b) {
+			l = a;
+			h = b;
+		} else {
+			h = a;
+			l = b;
+		}
+		int i = 2;
+		long lcm = h;
+		while(lcm % l != 0) {
+			//System.out.println("lcm is " + lcm + " i="+ i + " h=" + h + " l=" + l);
+			lcm = h * i++;
+			//System.out.println("new lcm is " + lcm + " i="+ i + " h=" + h + " l=" + l);
+		}
+		
+		// the second thing in the array is how much to multiple the HIGHER number to to get the lcm
+		return new long[] { lcm, i-1 };
+	}
+	
+	public long[] reduceFraction(long n, long d) {
+		
+		//int i = 2;
+		long na = n, da = d;
+		for(int i=2; i < d/2; i++) {
+			if(na % i == 0 && da % i == 0) {
+				while (na % i == 0 && da % i == 0) {
+					na /= i;
+					da /= i;	
+				}
+				i++;
+			}
+		}
+		
+		return new long[] {na, da};
+	}
 	
 	@Override
 	public void problem() {
 
-		System.out.println("Here ...");
-		
-		int max = 8;
-		int n = 2;
+		int max = 1000000;
+		int nGoal = 3, dGoal = 7;
 		
 		for(int i=max; i > 0; i--) { 	// denom
-			System.out.println(1 + " / " + i);
 			
+			if(i == dGoal)
+				continue;
 			
+			long[] dets  = lcmWithMult(i, dGoal);
 			
-			/*
-			for(int j=1; j<i; j++) { 	// numer
-				System.out.println(j + " / " + i);
-				
-				if(j != 1 && i % j == 0) {
-					System.out.println("Can reduce: " + j + "/" + i + " to 1/" + (i/j));
-				}
+			long w = dets[1];
+			if(dGoal < i) // the second thing in the array
+				// is how many to mult the HIGHER num to to get the lcm
+				// but we dont know if the i we are testing is higher / lower than dGoal
+				// so we need to do this only if dGoal is less than i
+				w = dets[0] / dGoal;
+			
+
+			long newN = (nGoal * w) - 1;
+			long[] red = reduceFraction(newN, dets[0]);
+			if(red[1] <= max) {
+				System.out.println("Fraction is " + red[0] + "/" + red[1]);
+				System.out.println("Answer: " + red[0]);
+				return;
 			}
-			*/
 			
 		}
-		
 		
 	}
 
