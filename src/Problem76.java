a28fd9658d22b2db52a8747bfccf9524675eced5
eljefeo <jeffnavabiz@yahoo.com>
2023-04-17 00:01:11 -0700

    notes 76

--- src/Problem76.java
+++ src/Problem76.java
@@ -15,7 +15,47 @@ public class Problem76 implements Problem{
 	
 	 */
 	
-
+	/*
+	 ways to make 5:
+	 	4 + 1
+		3 + 2
+		3 + 1 + 1
+		2 + 2 + 1
+		2 + 1 + 1 + 1
+		1 + 1 + 1 + 1 + 1
+		
+		reversed:
+		
+		2 + 3
+		1 + 4
+		1 + 1 + 3
+		1 + 2 + 2
+		1 + 1 + 1 + 2
+		1 + 1 + 1 + 1 + 1
+		
+		ways to make 4:
+		3 + 1
+		2 + 2
+		2 + 1 + 1
+		1 + 1 + 1 + 1
+		
+		reversed:
+		2 + 2
+		1 + 3
+		1 + 1 + 2
+		1 + 1 + 1 + 1
+		
+		ways to make 3:
+		2 + 1
+		1 + 1 + 1
+		
+		reversed:
+		1 + 2
+		1 + 1 + 1
+		
+		ways to make 2: 
+		1 + 1
+	 */
 	public static void main(String[] args) {
 		Problem p = new Problem76();
 		p.runProblem();
@@ -25,7 +65,54 @@ public class Problem76 implements Problem{
 	@Override
 	public Object problem() {
 		System.out.println("Here we go");
+		entry();
 		return null;
 	}
-
+	
+	private static void entry() {
+		
+		
+		int test = 5;
+		int ways = recur(test);
+		System.out.println("ways to make " + test + " : " + ways);
+	}
+	
+	private static int recur(int n) {
+		
+		if(n < 2) return 0;
+		
+		int waysToMakeN = 0;
+		System.out.println("starting recur with " + n);
+		int sub = 0;
+		while(sub < n) {
+			
+			
+			sub++;
+			
+			
+			
+			int diff = n - sub;
+			if(diff > 1) {
+				System.out.println("gonna call recur again for n: " + n + " with sub: " + sub + " diff: " + diff);
+				int newWays = recur(diff);
+				System.out.println("inside got this many ways to make " + diff + " with sub: " + sub + " : ways = " + newWays + " ::: waysToMakeN was " + waysToMakeN);
+				waysToMakeN += newWays;
+				System.out.println(" and now it is : " + waysToMakeN);
+			} else if (diff == 1){ //we end here this recur?
+				System.out.println("breaking at diff: " + diff + ", sub: " + sub + " for n: " + n);
+				waysToMakeN++;
+				break;
+			} else {
+				System.out.println("ERROR " + diff + ", sub: " + sub + " for n: " + n);
+			}
+			
+			
+			
+		}
+		
+		
+		System.out.println("returning " + waysToMakeN + " for n :" + n);
+		return waysToMakeN;
+				
+	}
 }
