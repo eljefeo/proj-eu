ce059f02692f8cc6dd2018c18da9f495c65ecaf7
eljefeo <jeffnavabiz@yahoo.com>
2023-04-21 17:02:51 -0700

    closer but too slow still

--- src/Problem76.java
+++ src/Problem76.java
@@ -1,5 +1,5 @@
 
-public class Problem76 implements Problem{// Took 2.596475 seconds ---- too SLOWW
+public class Problem76 implements Problem{// Took 1.581191 seconds ---- too SLOWW
 
 	/*
 	 It is possible to write five as a sum in exactly six different ways:
@@ -12,7 +12,7 @@ public class Problem76 implements Problem{// Took 2.596475 seconds ---- too SLOW
 		1 + 1 + 1 + 1 + 1
 		
 	How many different ways can one hundred be written as a sum of at least two positive integers?
-	
+	190569291
 	 */
 
 	public static void main(String[] args) {
@@ -22,17 +22,28 @@ public class Problem76 implements Problem{// Took 2.596475 seconds ---- too SLOW
 	
 	@Override
 	public Object problem() {
-		int test = 100;
-		return howManyWaysToSumToN(0, test, 1, 0, test-1);
+		int num = 100;
+		return howManyWaysToSumToN(0, num, 0, num-1);
 	}
 
-	public static int howManyWaysToSumToN(int n, int og, int digit, int count, int end) {
+	public static int howManyWaysToSumToN(int num, int goal,  int count, int end) {
+		if(num == goal) {
+			count++;
+		} else if(num < goal) {
+			for(int i = 1; i <= end; i++) {
+				count = howManyWaysToSumToN(num + i, goal, count, i);
+			}
+		}
+		return count;
+	}
+	
+	public static int howManyWaysToSumToN_original(int n, int og, int digit, int count, int end) {
 		if(n == og) {
 			count++;
 		} else if(n < og) {
 			int div = og/digit;
 			for(int i = 1; i <= end; i++) {
-				count = howManyWaysToSumToN(n + i, og, digit+1, count, i < div  ? i : div);
+				count = howManyWaysToSumToN_original(n + i, og, digit+1, count, i < div  ? i : div);
 			}
 		}
 		return count;
