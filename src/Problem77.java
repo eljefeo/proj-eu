a83e3c6377371d4741944c60d8fc103d28e9a076
eljefeo <jeffnavabiz@yahoo.com>
2023-04-23 11:04:09 -0700

    77 done using knowledge from 76

---
+++ src/Problem77.java
@@ -0,0 +1,29 @@
+
+public class Problem77 implements Problem{// Took 0.004933 seconds
+
+	/*
+	 It is possible to write ten as the sum of primes in exactly five different ways:
+
+		7 + 3
+		5 + 5
+		5 + 3 + 2
+		3 + 3 + 2 + 2
+		2 + 2 + 2 + 2 + 2
+		
+		What is the first value which can be written as the sum of primes in over five thousand different ways?
+	 */
+
+	public static void main(String[] args) {
+		Problem p = new Problem77();
+		p.runProblem();
+	}
+
+	
+	@Override
+	public Object problem() {
+		int limit = 5000;
+		
+		return Util.findNumWithThisManyPrimeSums(limit);
+	}
+	
+}
