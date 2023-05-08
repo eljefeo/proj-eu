09d841f1e7782a99a44082a2e03cf1318813b682
eljefeo <jeffnavabiz@yahoo.com>
2023-05-07 22:49:00 -0700

    use int for 78

--- src/Problem78.java
+++ src/Problem78.java
@@ -8,15 +8,56 @@ public class Problem78 implements Problem{// Took 0.026811 seconds
 		Problem p = new Problem78();
 		p.runProblem();
 	}
-	//static int t = 0;
+
 	@Override
 	public Object problem() {
-		//System.out.println("mil : " + howManyWaysToSumToNEulers_divisibleMillion2());
-		return howManyWaysToSumToNEulers_divisibleMillion2();
-		
+		int million = 1000000;
+		return howManyWaysToSumToNEulers_divisibleByN(million);
 	}
 	
-	public int howManyWaysToSumToNEulers_divisibleMillion() {
+	public int howManyWaysToSumToNEulers_divisibleByN(int numDivisible) {
+		List<Integer> all = new ArrayList<Integer>();
+		all.add(1);
+		all.add(1);
+		int ii = 2;
+		while(true) {
+			int run = 0;
+			int neg = -1;
+			int posNeg = 1;
+			int odd = 1;
+			int nat = 1;
+			int nToDo = ii;
+			boolean shouldSubOdd = false;
+			int numToSub = odd;
+			while(true) {
+				nToDo = nToDo - numToSub ;
+				if(nToDo < 0 ) {
+					break;
+				}
+				run += (all.get(nToDo) * posNeg);
+				if(shouldSubOdd = !shouldSubOdd) {
+					odd += 2;//make all odd numbers, 1 3 5 7 9 etc..
+					numToSub = nat;
+				} else {
+					nat++;//make all natural numbers, 1 2 3 4 5 6 7 ...
+					numToSub = odd;
+					posNeg *= neg;
+				}
+			}
+			run %= numDivisible; //we can store only the last 6 digits because 
+				//if we add or multiply these together, the numbers after the first 6 digits dont matter or have any effect
+			if(run == 0) {
+				return ii;
+			}
+			all.add(run);
+			ii++;
+		}
+	}
+	
+
+	//this one can be helpful if you actually want to see all the digits in the num of partitions. the one above only uses the last 6 to solve problem
+	// but this one actually gives the num of partitions of a num
+	public int howManyWaysToSumToNEulers_divisibleMillion_BigInt() {
 		List<BigInteger> all = new ArrayList<BigInteger>();
 		all.add(new BigInteger("1"));
 		all.add(new BigInteger("1"));
@@ -57,48 +98,5 @@ public class Problem78 implements Problem{// Took 0.026811 seconds
 			ii++;
 		}
 	}
-	
-	public int howManyWaysToSumToNEulers_divisibleMillion2() {
-		List<Integer> all = new ArrayList<Integer>();
-		all.add(1);
-		all.add(1);
-		
-		int million = 1000000;
-		int ii = 2;
-		while(true) {
-			int run = 0;
-			int  neg = -1;
-			int  posNeg = 1;
-			int odd = 1;
-			int nat = 1;
-			int nToDo = ii;
-			boolean shouldSubOdd = false;
-			int numToSub = odd;
-			while(true) {
-				nToDo = nToDo - numToSub ;
-				if(nToDo < 0 ) {
-					break;
-				}
-				run += (all.get(nToDo) * posNeg);
-				if(shouldSubOdd = !shouldSubOdd) {
-					odd += 2;//make all odd numbers, 1 3 5 7 9 etc..
-					numToSub = nat;
-				} else {
-					nat++;//make all natural numbers, 1 2 3 4 5 6 7 ...
-					numToSub = odd;
-					posNeg *= neg;
-				}
-			}
-			run %= million; //we can store only the last 6 digits because 
-				//if we add or multiply these together, the numbers after the first 6 digits dont matter or have any effect
-			if(run == 0) {
-				System.out.println("ans: " + ii + " : " + run);
-				return ii;
-			}
-			all.add(run);
-			ii++;
-		}
-	}
-	
 	
 }
