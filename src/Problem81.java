77a8936b8cd3bd9bc3a674137921e51ff83c622f
eljefeo <jeffnavabiz@yahoo.com>
2023-05-09 23:06:48 -0700

    messing with beginning 81

--- src/Problem81.java
+++ src/Problem81.java
@@ -9,10 +9,11 @@ public class Problem81 implements Problem{// Took 0.018977 seconds
 
 	
 	/*
-	In the 5 by 5 matrix below, the minimal path sum from the top left to the bottom right, by only moving to the right and down, is indicated in bold red and is equal to 2427.
+	In the 5 by 5 matrix below, the minimal path sum from the top left to the bottom right, 
+		by only moving to the right and down, is indicated in bold red and is equal to 2427.
 
 		131*	673		234		103		18
-		210*	96*		342*	965		105
+		201*	96*		342*	965		150
 		630		803		746*	422*	111
 		537		699		497		121*	956
 		805		732		524		37*		331*
@@ -28,31 +29,57 @@ Find the minimal path sum from the top left to the bottom right by only moving r
 
 	@Override
 	public Object problem() {
-		return run();
+		return goBackwards();
 	}
+	/*
+	 original:
+		{131,	673,	234,	103,	18},
+		{201,	96,		342,	965,	150},
+		{630,	803,	746,	422,	111},
+		{537,	699,	497,	121,	956},
+		{805,	732,	524,	37,		331}
+	 */
 	
+	//maybe need to look 2 or 3 steps ahead? like if you can check up 1 and up 2 and both of those lefts
 	
-	public static int run() {
+	public static int goBackwards() {
 		
 		int[][] nums = new int[][]{
-				{131,	673,	234,	103,	18},
-				{210,	96,		342,	965,	105},
-				{630,	803,	746,	422,	111},
-				{537,	699,	497,	121,	956},
+				{7,		6,		5,		4,		3},
+				{201,	96,		342,	965,	2},
+				{630,	803,	746,	422,	1},
+				{537,	699,	497,	121,	38},
 				{805,	732,	524,	37,		331}
 			};
 			
+			//nums = Problem81Helper.getNums();
+			long total = nums[0][0];
 			int xl = nums[0].length;
 			int yl = nums.length;
 			for(int i=yl-1; i > 0; i--) {
 				
-				for(int j=xl-1; j > 0; j--) {
+				for(int j=xl-1; j >= 0; j--) {
+					System.out.print("i=" + i + " j=" + j );
+					total += nums[i][j];
+					System.out.println(" added: " + nums[i][j] + " total=" + total);
 					
-					System.out.println("i=" + i + " j=" + j);
+					if(i == 0) {
+						//i--;
+						continue;
+					}
+					if(j == 0) {
+						continue;
+					} 
 					int left = nums[i][j-1];
 					int up = nums[i-1][j];
-					System.out.println("at row xy: " + nums[i][j] + " left=" + left + ", up=" + up);
-					
+					//System.out.println("at row xy: " + nums[i][j] + " left=" + left + ", up=" + up);
+					if(up < left) {
+						i--;
+						j++;
+						//System.out.println(up + " < " + left + "	switched to: " + nums[i][j] + " left=" + left + ", up=" + up);
+					} else {
+						//System.out.println(left + " < " + up + "	naturally switched to: " + nums[i][j] + " left=" + left + ", up=" + up);
+					}
 				}
 				
 			}
@@ -60,6 +87,50 @@ Find the minimal path sum from the top left to the bottom right by only moving r
 		return 0;
 	}
 
+	public static int goForwards() {
+		
+		int[][] nums = new int[][]{
+				{131,	673,	234,	103,	18},
+				{201,	96,		342,	965,	150},
+				{630,	803,	746,	422,	111},
+				{537,	699,	497,	121,	956},
+				{805,	732,	524,	37,		331}
+			};
+			
+			//nums = Problem81Helper.getNums();
+			long total = nums[0][0];
+			int xl = nums[0].length;
+			int yl = nums.length;
+			for(int i=0; i < yl; i++) {
+				
+				for(int j=0; j < xl; j++) {
+					System.out.print("i=" + i + " j=" + j );
+					total += nums[i][j];
+					System.out.println(" added: " + nums[i][j] + " total=" + total);
+					
+					if(i == 0) {
+						//i--;
+						continue;
+					}
+					if(j == 0) {
+						continue;
+					} 
+					int left = nums[i][j-1];
+					int up = nums[i-1][j];
+					//System.out.println("at row xy: " + nums[i][j] + " left=" + left + ", up=" + up);
+					if(up < left) {
+						i--;
+						j++;
+						//System.out.println(up + " < " + left + "	switched to: " + nums[i][j] + " left=" + left + ", up=" + up);
+					} else {
+						//System.out.println(left + " < " + up + "	naturally switched to: " + nums[i][j] + " left=" + left + ", up=" + up);
+					}
+				}
+				
+			}
+		
+		return 0;
+	}
 	
 	
 }
